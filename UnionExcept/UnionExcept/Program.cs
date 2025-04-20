d204a71cbb3bf36ab5c3f272ac1f5e8babb6a34b
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-04-20 14:35:57 +0200

    Add New Project

---
+++ UnionExcept/UnionExcept/Program.cs
@@ -0,0 +1,53 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: Method for Union and Except between two arrays
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+namespace UnionExcept
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Union");
+            Console.WriteLine("===========");
+            Console.WriteLine("Normalfall");
+            int[] feldA = { 1, 1, 3, 5, 7, 9, 1, 3, };
+            int[] feldB = { 0, 1, 2, 3 };
+            SetTools.PrintMessage(feldA, feldB, "union");
+
+            Console.WriteLine("Alle gleich");
+            feldA = new [] { 1, 1, 3, 5, };
+            feldB = new[] { 1, 3, 3, 5, };
+            SetTools.PrintMessage(feldA, feldB, "union");
+
+            Console.WriteLine("Keine Überschneidung");
+            feldA = new[] { 1, 3, 5, 7, 9 };
+            feldB = new[] { 0, 2, 4, 6, 8 };
+            SetTools.PrintMessage(feldA, feldB, "union");
+
+            Console.WriteLine("Exept");
+            Console.WriteLine("===========");
+            Console.WriteLine("Normalfall");
+            feldA = new [] { 1, 1, 3, 5, 7, 9, 1, 3, };
+            feldB = new [] { 0, 1, 2, 3 };
+            SetTools.PrintMessage(feldA, feldB, "except");
+
+            Console.WriteLine("Alle gleich");
+            feldA = new[] { 1, 1, 3, 5, };
+            feldB = new[] { 1, 3, 3, 5, };
+            SetTools.PrintMessage(feldA, feldB, "except");
+
+            Console.WriteLine("Keine Überschneidung");
+            feldA = new[] { 1, 3, 5, 7, 9 };
+            feldB = new[] { 0, 2, 4, 6, 8 };
+            SetTools.PrintMessage(feldA, feldB, "except");
+        }
+    }
+}
---
+++ UnionExcept/UnionExcept/SetTools.cs
@@ -0,0 +1,156 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: Method for Union and Except between two arrays
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+namespace UnionExcept
+{
+    public class SetTools
+    {
+        public static int[] Union(int[] firstArray, int[] secondArray)
+        {
+            int size = firstArray.Length + secondArray.Length;
+            int[] modifiedArray = new int[size];
+            int i;
+            for (i = 0; i < size - secondArray.Length; i++)
+            {
+                modifiedArray[i] = firstArray[i];
+            }
+            int j = 0;
+            for (; i < size; i++)
+            {
+                modifiedArray[i] = secondArray[j];
+                j++;
+            }
+            return RemoveDuplicate(modifiedArray);
+        }
+        private static int[] RemoveDuplicate(int[] array)
+        {
+            int size = array.Length;
+            int[] modifiedArray = new int[size];
+            int index = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                bool isSame = false;
+                for (int j = 0; j < index; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        isSame = true;
+                    }
+                }
+                if (!isSame)
+                {
+                    bool alreadyAdded = false;
+
+                    for (int n = 0; n < index; n++)
+                    {
+                        if (modifiedArray[n] == array[i])
+                        {
+                            alreadyAdded = true;
+                        }
+                    }
+                    if (!alreadyAdded)
+                    {
+                        modifiedArray[index] = array[i];
+                        index++;
+                    }
+                }
+            }
+            return ConcateZero(index, modifiedArray);
+        }
+        public static int[] Except(int[] firstArray, int[] secondArray)
+        {
+            int size = firstArray.Length + secondArray.Length;
+            int[] modifiedArray = new int[size];
+            int index = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                bool isFirst = true;
+                for (int j = 0; j < secondArray.Length; j++)
+                {
+                    if (firstArray[i] == secondArray[j])
+                    {
+                        isFirst = false;
+                    }
+                }
+                if (isFirst)
+                {
+                    modifiedArray[index] = firstArray[i];
+                    index++;
+                }
+            }
+            int[] outputArray = RemoveDuplicate(modifiedArray);
+            int[] result = ConcateZero(index,  outputArray);
+            return result;
+        }
+        private static int[] ConcateZero(int size, int[] array)
+        {
+            int[] outputArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                outputArray[i] = array[i];
+            }
+            return outputArray;
+        }
+        public static void PrintMessage(int[] firstArray, int[] secondArray, string methodName)
+        {
+            if (methodName.ToLower() == "union")
+            {
+                int[] array = Union(firstArray, secondArray);
+                Console.Write("Feld A: ");
+                foreach (int i in firstArray)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+                Console.Write("Feld B: ");
+                foreach (int j in secondArray)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+                Console.Write($"Ziffern in Feld A und Feld B: ");
+
+                foreach (int i in array)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("===========");
+            }
+            if (methodName.ToLower() == "except")
+            {
+                int[] array = Except(firstArray, secondArray);
+                Console.Write("Feld A: ");
+                foreach (int i in firstArray)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+                Console.Write("Feld B: ");
+                foreach (int j in secondArray)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+                Console.Write($"Ziffern in Feld A und Feld B: ");
+
+                foreach (int i in array)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("===========");
+            }
+        }
+    }
+}
